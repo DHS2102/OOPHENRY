b223757b8762771793328cf9c2115d27703c8321
DHS2102 <dahuertass@udistrital.edu.co>
2025-10-22 19:08:58 -0500

    Delete Corte1/PracticasIniciales/CalculadoraJava directory

--- Corte1/PracticasIniciales/CalculadoraJava/AppSuma.java
+++
@@ -1,9 +0,0 @@
-public class AppSuma {
-    public static void main(String[] args) throws Exception {
-        Modelo modelo = new Modelo();
-        Vista vista = new Vista();
-        Controlador controlador = new Controlador(modelo, vista);
-        controlador.iniciar();
-    
-    } 
-}
